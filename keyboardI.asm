f7650e03f7db8d621968f463cb63b877aefa570c
chenyinheng30 <chenyinheng30@Outlook.com>
2018-10-01 22:45:43 +0800

    定义了keyboardI.asm的基本属性

--- keyboardI.asm
+++ keyboardI.asm
@@ -1,1 +1,15 @@
-;
+;键盘输入
+
+;SEGMENT(dw)
+;OFFSET(dw)
+
+;--->io
+;-->INPUT i
+
+;--->keyboardI
+;--'>KEYBOARD_INPUT:INPUT(SEGMENT) *input
+;+-->STRING(OFFSET) *string
+;+--private'>TEXT_SCREEN(OFFSET) *screen
+;++-->SEGMENT	input_starting
+;++-->OFFSET cursor
+;--->
