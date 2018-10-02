4cf19508440ad195528f78e8b447f502f1972c52
chenyinheng30 <chenyinheng30@Outlook.com>
2018-10-02 11:24:11 +0800

    完成keyboardI.asm的KEYBOARD_INPUT段

--- keyboardI.asm
+++ keyboardI.asm
@@ -2,14 +2,28 @@
 
 ;SEGMENT(dw)
 ;OFFSET(dw)
+;BASIC(OFFSET)
+;LABEL
+;SEGMENT-LABEL
 
 ;--->io
 ;-->INPUT i
 
 ;--->keyboardI
-;--'>KEYBOARD_INPUT:INPUT(SEGMENT) *input
-;+-->STRING(OFFSET) *string
-;+--private'>TEXT_SCREEN(OFFSET) *screen
+;--'>SEGMENT-LABEL KEYBOARD_INPUT:INPUT
+;+-->STRING(BASIC) *string
+;+-->OFFSET ending
+;+-->OFFSET current
+;+--private'>LABEL TEXT_SCREEN
 ;++-->SEGMENT	input_starting
 ;++-->OFFSET cursor
-;--->
+;--'>
+SECTION KEYBOARD_INPUT vstart=0;-->:INPUT
+	string dw 00
+	ending dw 00
+	current dw 00
+	TEXT_SCREEN:
+		input_starting dw 00
+		cursor dw 00
+SEGMENT CODE vstart=0
+	
